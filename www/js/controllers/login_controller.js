9d2d639aeb50dc9f64296c3010f9b2b02e70f986
Ausin Formica <austin.formica@gmail.com>
2015-07-24 13:16:05 -0400

    login prompt for chat state

--- www/js/controllers/login_controller.js
+++ www/js/controllers/login_controller.js
@@ -1,3 +1,11 @@
 app.controller('LoginCtrl', function($state, $sanitize) {
+  var self = this;
+
+  self.join = function() {
+    var nickname = $sanitize(self.nickname);
+    if (nickname) {
+      $state.go('chat',{nickname:nickname})
+    }
+  }
 
 });
