cde9ed042c059de8a1bf079460dd540f49937a29
Ausin Formica <austin.formica@gmail.com>
2015-07-24 13:17:40 -0400

    create socket service

---
+++ www/js/services/socket.js
@@ -0,0 +1,10 @@
+app.factory('socket',function(socketFactory){
+
+  var myIoSocket = io.connect('http://chat.socket.io');
+
+  mySocket = socketFactory({
+    ioSocket: myIoSocket
+  });
+
+  return mySocket;
+})
