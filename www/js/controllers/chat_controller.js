860b988c706529b6bacd726e49bc746ee505464b
Ausin Formica <austin.formica@gmail.com>
2015-07-24 13:35:59 -0400

    hook up chat ctrl

--- www/js/controllers/chat_controller.js
+++ www/js/controllers/chat_controller.js
@@ -1,3 +1,15 @@
-app.controller('ChatCtrl', function($state){
+app.controller('ChatCtrl', function($state, $scope, socket){
+
+  $scope.$on('$ionicView.enter', function() {
+    console.log("chat loaded");
+  });
+
+  socket.on('connect', function() {
+    sokcet.emti('add user', `nickname`);
+  })
+
+  $scope.goLogin = function() {
+    $state.go('login');
+  }
 
 });
